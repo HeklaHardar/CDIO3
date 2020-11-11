e34daaec1f8b20c423243e07d751700b4e3ed89a
Mikkel <mikkelsa97@gmail.com>
2020-11-11 14:41:33 +0100

    Changed variable name of the cards array

--- src/main/java/Game/CardPicker.java
+++ src/main/java/Game/CardPicker.java
@@ -5,7 +5,7 @@ public class CardPicker {
     private int random1;
     private int random2;
     private int temp;
-    private int[] ints = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    private int[] cards = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
     private int Counter = -1;
     private int first;
     Cards card = new Cards();
@@ -13,11 +13,11 @@ public class CardPicker {
     //Randomises card order
     public void CardScrambler(){
         for (int i = 0; i < 1000 - 1; i++) {
-            temp = ints[random1 = (int) (Math.random() * (ints.length))];
-            ints[random1] = ints[random2 = (int) (Math.random() * (ints.length))];
-            ints[random2] = temp;
+            temp = cards[random1 = (int) (Math.random() * (cards.length))];
+            cards[random1] = cards[random2 = (int) (Math.random() * (cards.length))];
+            cards[random2] = temp;
         }
-        for (int i: ints
+        for (int i: cards
              ) {
             System.out.print(i + " ");
         }
@@ -28,12 +28,12 @@ public class CardPicker {
 
         //Blah Blah noget med at trække et kort fra en anden klasse her
 
-        first = ints[0];
-        for (int i = 0; i < ints.length - 1;i++)
-            ints[i] = ints[i+1];
-        ints[ints.length - 1] = first;
+        first = cards[0];
+        for (int i = 0; i < cards.length - 1;i++)
+            cards[i] = cards[i+1];
+        cards[cards.length - 1] = first;
 
-        for (int i:ints
+        for (int i:cards
              ) {
             System.out.print(i + " ");
         }
