22ff82d5b9383ed3e7c49eef89f56ac1b108b7ff
Mikkel <mikkelsa97@gmail.com>
2020-11-11 15:13:56 +0100

    Player class now has currentPosition and updatePosition

--- src/main/java/Game/Player.java
+++ src/main/java/Game/Player.java
@@ -3,6 +3,7 @@ package Game;
 public class Player {
 
     private int score;
+    private int playerPosition = 0;
 
     // Determines variables
     Account account = new Account();
@@ -22,5 +23,9 @@ public class Player {
 
     //Updates the currentplayers score
     public void playerBalanceUpdate(int update){account.updateScore(update);}
+
+    public int currentPosition(){return playerPosition;}
+
+    public void updatePosition(int position){playerPosition+=position;}
 
 }
