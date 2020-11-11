a58b106389b0178e10d1d8119ac9198f4b0b46c2
Mikkel <mikkelsa97@gmail.com>
2020-11-11 14:37:24 +0100

    Takes the drawn card and puts it 'at the bottom' of the pile

--- src/main/java/Game/CardPicker.java
+++ src/main/java/Game/CardPicker.java
@@ -7,9 +7,10 @@ public class CardPicker {
     private int temp;
     private int[] ints = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
     private int Counter = -1;
+    private int first;
     Cards card = new Cards();
 
-
+    //Randomises card order
     public void CardScrambler(){
         for (int i = 0; i < 1000 - 1; i++) {
             temp = ints[random1 = (int) (Math.random() * (ints.length))];
@@ -20,7 +21,22 @@ public class CardPicker {
              ) {
             System.out.print(i + " ");
         }
+    }
+    //Draws a card
+    public void DrawCard(){
+        System.out.println(" ");
 
+        //Blah Blah noget med at trække et kort fra en anden klasse her
+
+        first = ints[0];
+        for (int i = 0; i < ints.length - 1;i++)
+            ints[i] = ints[i+1];
+        ints[ints.length - 1] = first;
+
+        for (int i:ints
+             ) {
+            System.out.print(i + " ");
+        }
     }
 
 
