73cdaac93ac36b49443434f66da6d5c0b7884476
Mikkel <mikkelsa97@gmail.com>
2020-11-11 10:23:56 +0100

    Made development branch

--- src/main/java/hej.java
+++ src/main/java/hej.java
@@ -1,12 +1,15 @@
+import gui_fields.*;
 import gui_main.GUI;
 
 public class hej {
 
     public static void main(String[] args) {
 
-        GUI gui = new GUI();
+        GUI_Player player = new GUI_Player("Mikkel",2000);
 
+        GUI_Field[] fields = {new GUI_Start(),new GUI_Street(),new GUI_Chance(),new GUI_Jail(),new GUI_Shipping(),new GUI_Brewery(), new GUI_Refuge()};
 
+        GUI gui = new GUI(fields);
+        GUI_Field[] field = gui.getFields();
     }
-
 }
