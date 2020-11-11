21c6f4ba3ad04cd4428cc950902c2b3edec9e9cc
Mikkel <mikkelsa97@gmail.com>
2020-11-11 10:39:28 +0100

    Imported classes from cdio2

--- src/main/java/hej.java
+++ src/main/java/hej.java
@@ -6,7 +6,6 @@ public class hej {
     public static void main(String[] args) {
 
         GUI_Player player = new GUI_Player("Mikkel",2000);
-
         GUI_Field[] fields = {new GUI_Start(),new GUI_Street(),new GUI_Chance(),new GUI_Jail(),new GUI_Shipping(),new GUI_Brewery(), new GUI_Refuge()};
 
         GUI gui = new GUI(fields);
