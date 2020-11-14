02694970d21287a54f83a14c2dcf58426b13a632
Mikkel <s205426@student.dtu.dk>
2020-11-14 15:10:15 +0100

    Started Drawcard function in cardpicker

--- src/main/java/Game/CardPicker.java
+++ src/main/java/Game/CardPicker.java
@@ -5,8 +5,9 @@ public class CardPicker {
     private int random1;
     private int random2;
     private int temp;
-    private int[] ints = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    private int[] cards = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
     private int Counter = -1;
+    private int first;
 
     private String testString;
     Cards card = new Cards();
@@ -17,15 +18,31 @@ public class CardPicker {
 
     public void CardScrambler(){
         for (int i = 0; i < 1000 - 1; i++) {
-            temp = ints[random1 = (int) (Math.random() * (ints.length))];
-            ints[random1] = ints[random2 = (int) (Math.random() * (ints.length))];
-            ints[random2] = temp;
+            temp = cards[random1 = (int) (Math.random() * (cards.length))];
+            cards[random1] = cards[random2 = (int) (Math.random() * (cards.length))];
+            cards[random2] = temp;
         }
-        for (int i: ints
+        for (int i: cards
              ) {
             System.out.print(i + " ");
         }
 
+    }
+
+    public void DrawCard(){
+        System.out.println(" ");
+
+        //Blah Blah noget med at trække et kort fra en anden klasse her
+
+        first = cards[0];
+        for (int i = 0; i < cards.length - 1;i++)
+            cards[i] = cards[i+1];
+        cards[cards.length - 1] = first;
+
+        for (int i:cards
+        ) {
+            System.out.print(i + " ");
+        }
     }
 
 
