bfc709300f48e80f24dbaeec2ab5f182426edb11
lau.thomsen.privat@gmail.com <lau.thomsen.privat@gmail.com>
2020-11-19 11:24:27 +0100

    Made some prison methods

--- src/main/java/Game/Controller/Game.java
+++ src/main/java/Game/Controller/Game.java
@@ -37,18 +37,23 @@ public class Game {
             for (int i = 0; i <= menu.getPlayerAmount() - 1; i++) {
                 juniorGui.gui.getUserString("tryk enter for at slå");
                 die.roll();
+                //Moves the car on the GUI and checks if player is over start.
                 juniorGui.moveCars(i, player[i].currentPosition(), player[i].updatePosition(die.getValue()));
+                //Subtracts money from the currentplayer and gives money to the player owning the field
                 properties.Fieldproperties(player[i].currentPosition());
-                if(properties.getOwnedFields()[player[i].currentPosition()] != i+1){
-                    player[i].playerBalanceUpdate(0-properties.getValue());
-                    if(properties.getOwnedFields()[player[i].currentPosition()]!=0) {
-                        player[properties.getOwnedFields()[player[i].currentPosition()]-1].playerBalanceUpdate(properties.getValue());
-                        juniorGui.updateGuiBalance(properties.getOwnedFields()[player[i].currentPosition()]-1, player[properties.getOwnedFields()[player[i].currentPosition()]-1].playerBalance());
+                if (properties.getOwnedFields()[player[i].currentPosition()] != i + 1) {
+                    player[i].playerBalanceUpdate(0 - properties.getValue());
+
+                    if (properties.getOwnedFields()[player[i].currentPosition()] != 0) {
+                        player[properties.getOwnedFields()[player[i].currentPosition()] - 1].playerBalanceUpdate(properties.getValue());
+                        juniorGui.updateGuiBalance(properties.getOwnedFields()[player[i].currentPosition()] - 1, player[properties.getOwnedFields()[player[i].currentPosition()] - 1].playerBalance());
                     }
                 }
                 juniorGui.updateGuiBalance(i, player[i].playerBalance());
-                juniorGui.landOnField(i,player[i].currentPosition(),player[i].playerString(),properties.getOwningStatus(), properties.getOwnedFields());
-                properties.setOwnedFields(properties.getOwnedFields(),player[i].currentPosition(),i);
+
+                juniorGui.landOnField(i, player[i].currentPosition(), player[i].playerString(), properties.getOwningStatus(), properties.getOwnedFields());
+
+                properties.setOwnedFields(properties.getOwnedFields(), player[i].currentPosition(), i);
             }
         }
 
